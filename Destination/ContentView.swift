f50efdaed40f6a70f7a83d66485e44e72e6d0698
Yuxuan Huang <huangyuxuan0325@126.com>
2025-07-07 09:52:04 +0800

    Final Submission

--- Destination/ContentView.swift
+++ Destination/ContentView.swift
@@ -7,13 +7,13 @@
 import SwiftUI
 
 struct ContentView: View {
-// Array of 5 destinations as required
-let destinations = ["Paris", "New York", "Hawaii", "Tokyo", "Rome"]
-
-// State properties
-@State private var destinationStr = "Where would you like to go?"
-@State private var selection = 0
-
+    // Array of 5 destinations as required
+    let destinations = ["Paris", "New York", "Nanjing", "Tokyo", "Rochester"]
+    
+    // State properties
+    @State private var destinationStr = "Where would you like to go?"
+    @State private var selection = 0
+    
     var body: some View {
         VStack {
             Spacer()
@@ -60,24 +60,29 @@ let destinations = ["Paris", "New York", "Hawaii", "Tokyo", "Rome"]
         }
         .padding()
     }
-
-// Function to handle Next button tap
-private func nextDestination() {
-selection = (selection + 1) % destinations.count
-destinationStr = "Where would you like to go?"
-}
+    
+    // Function to handle Next button tap
+    private func nextDestination() {
+        selection = (selection + 1) % destinations.count
+        destinationStr = "Where would you like to go?"
+    }
     
     // Function to handle Pick button tap
     private func pickDestination() {
-        destinationStr = "Congratulations - your trip to \(destinations[selection]) has been confirmed!"
+        if(destinations[selection]) == "Nanjing"{
+            destinationStr = "Congratulations - your trip to \(destinations[selection]) has been confirmed! Welcome to my hometown!"
+        }else{
+            destinationStr = "Congratulations - your trip to \(destinations[selection]) has been confirmed!"
         }
-        }
+        
+    }
+}
 
-        struct ContentView_Previews: PreviewProvider {
-        static var previews: some View {
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
         ContentView()
-        }
-        }
+    }
+}
     
 #Preview {
     ContentView()
